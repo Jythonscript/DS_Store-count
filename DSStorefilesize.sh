16b47c869f2a5e36f5c9c63f185b979b3c19d5bf
Jythonscript <90302927@ep-student.org>
2018-11-28 21:16:58 -0600

    add initial shell file

---
+++ DSStorefilesize.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export dir="`dirname ~`/`whoami`"
+export sum=0
+export numfiles=0
+export file=".temp-dsstore.txt"
+export file2=".temp-dsstore2.txt"
+
+find $dir -type f -name ".DS_Store" -exec du -k {} \; > $file
+cat $file | sed -e "s/\([0-9]*\).*$/\1/g" > $file2
+cat $file2 > $file
+
+while IFS= read -r line; do
+	sum=$(expr $(expr 0 \+ $sum) \+ $(echo $line | xargs -I{} expr 0 \+ {}))
+	numfiles=$(expr 1 \+ $numfiles)
+	#echo $line
+done < "$file"
+rm $file
+rm $file2
+echo $numfiles" DS_Store files were found in "$dir
+echo "They add up to "$sum"K in size"
