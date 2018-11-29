25d6a58e5a5a47f24dbec408736d2ef3f8af13a4
Jythonscript <90302927@ep-student.org>
2018-11-29 12:02:04 -0600

    remove unnecessary redirection for speed

--- DSStorefilesize.sh
+++ DSStorefilesize.sh
@@ -7,12 +7,11 @@ export file2=".temp-dsstore2.txt"
 
 find $dir -type f -name ".DS_Store" -exec du -k {} \; > $file
 cat $file | sed -e "s/\([0-9]*\).*$/\1/g" > $file2
-cat $file2 > $file
 
 while IFS= read -r line; do
 	sum=$(expr $(expr 0 \+ $sum) \+ $(echo $line | xargs -I{} expr 0 \+ {}))
 	numfiles=$(expr 1 \+ $numfiles)
-done < "$file"
+done < "$file2"
 
 rm $file
 rm $file2
